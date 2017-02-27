59063a50ae29a70061c0ed3d2b1296bf372027f2
4quet <louis@soixantecircuits.fr>
2017-02-27 17:18:29 +0100

    Using spacebro instead of socket.io

--- index.js
+++ index.js
@@ -3,14 +3,21 @@
 const express = require('express')
 const app = express()
 const http = require('http').Server(app)
-const io = require('socket.io')(http)
 const path = require('path')
 const execa = require('execa')
 const chokidar = require('chokidar')
+const spacebroClient = require('./spacebro-client')
 
 const config = require('./config/config')
 
 app.use('/media', express.static(config.mediaFolder))
+app.use('/spacebro-client', express.static('spacebro-client'))
+
+spacebroClient.connect('127.0.0.1', 8888, {
+  clientName: 'fast-print-ui-back',
+  channelName: 'fast-print',
+  verbose: false
+})
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'index.html'))
@@ -18,22 +25,21 @@ app.get('/', function (req, res) {
 
 http.listen(config.port, function () {
   console.log('Fast Print UI: Listening on port', config.port)
-})
+  spacebroClient.on('ready', function () {
+    var watcher = chokidar.watch(config.mediaFolder)
+    watcher
+    .on('add', filepath => spacebroClient.emit('add-item', path.basename(filepath)))
+    .on('unlink', filepath => spacebroClient.emit('delete-item', path.basename(filepath)))
 
-io.on('connection', function (socket) {
-  var watcher = chokidar.watch(config.mediaFolder)
-  watcher
-  .on('add', filepath => socket.emit('addItem', path.basename(filepath)))
-  .on('unlink', filepath => socket.emit('deleteItem', path.basename(filepath)))
-
-  socket.on('print', function (data) {
-    var file = path.join(config.mediaFolder, path.basename(data.media))
-    execa('lp', [ '-d', config.printer,
-      '-o', 'media=' + config.format,
-      '-n', '1', file])
-    .then(res => {
-      console.log('Printing', data.media, 'with', config.printer)
-      console.log(res.stdout)
-    }).catch(error => console.log(error.stderr))
+    spacebroClient.on('print', function (data) {
+      var file = path.join(config.mediaFolder, path.basename(data.media))
+      execa('lp', [ '-d', config.printer,
+        '-o', 'media=' + config.format,
+        '-n', '1', file])
+      .then(res => {
+        console.log('Printing', data.media, 'with', config.printer)
+        console.log(res.stdout)
+      }).catch(error => console.log(error.stderr))
+    })
   })
 })
